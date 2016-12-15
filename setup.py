0002bf42fc8801d3afb4b8621e25a52c3440e0e2
Pontus Enmark <pontus@wka.se>
2016-12-15 16:10:21 +0100

    Add requirements to setup

--- setup.py
+++ setup.py
@@ -15,5 +15,14 @@ setup(
             ingest = ingest:from_cmd_line
             ingest_mediainfo = ingest.mediainfo:from_cmd_line
         """
-    }
+    },
+    install_requires="""
+        gevent==1.1.2
+        lxml==3.4.4
+        pillow==3.3.1
+        pymongo==3.0.3
+        python-magic==0.4.12
+        s3_wrapper
+        python-dotenv==0.6.2
+    """
 )
