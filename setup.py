00ee7e12500020e0ca9889bfb0547676e1430f07
Pontus Enmark <pontus@wka.se>
2017-01-23 09:02:58 +0100

    Bump 1.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ingest',
     description='mongodb mediainfo ingester',
-    version='1.0.2',
+    version='1.0.4',
     packages=find_packages(exclude=['ingest.tests']),
     tests_require=['nose'],
     test_suite='ingest.tests',
